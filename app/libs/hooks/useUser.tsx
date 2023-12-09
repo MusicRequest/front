3a5d9372d9dfec263458ee8ffc5803b9ec241994
FlorianBEME <florianbme@gmail.com>
2023-12-09 23:05:41 +0100

    Création d'un hook de gestion du current Visiteur

---
+++ app/libs/hooks/useUser.tsx
@@ -0,0 +1,32 @@
+import { useUserStore } from "@/store/userStore";
+import fetchData from "../utils/fetch";
+import baseUrl from "../utils/urlApi";
+
+export default function useUser() {
+  const { user, setUser } = useUserStore();
+
+  const verifyUser = async (eventID: string) => {
+    if (user && eventID) {
+      const USER_ID = user.id;
+      try {
+        await fetchData(
+          baseUrl(`/visitor/${USER_ID}/verify/${eventID}`),
+          "GET"
+        );
+      } catch (err) {
+        console.log(err);
+        throw new Error();
+      }
+    }
+  };
+
+  const saveVisitor = (param: User) => {
+    setUser(param);
+  };
+
+  const removeOlderUser = async () => {
+    // TODO: A intégrer
+  };
+
+  return { user, verifyUser, saveVisitor };
+}
