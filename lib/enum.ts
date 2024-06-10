cbdb43ba63f92d9e4eb45f9022ddc64a707e1d12
FlorianBEME <florianbme@gmail.com>
2024-06-10 22:26:53 +0200

    Ajout enum

---
+++ lib/enum.ts
@@ -0,0 +1,12 @@
+export enum MessageDefault {
+  Update = "L'élément a bien été modifié. ",
+  Create = "L'élément a bien été créé. ",
+  Delete = "L'élément a bien été supprimé. ",
+  Error = "Une erreur est survenue. Si cette erreur persiste veuillez contacter le support technique.",
+  Succes = "Votre demande a été prise en compte.",
+}
+
+export enum EventOptions {
+  WallPicture = "wall-picture",
+  MusicRequest = "music-request",
+}
